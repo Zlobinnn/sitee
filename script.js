6e07bf340876c8b3550bf025746d968c18b21270
Zlobinnn <zlobin.savva@mail.ru>
2023-02-19 18:20:06 +0300

    1

--- script.js
+++ script.js
@@ -1,40 +1,63 @@
-var photo = document.getElementById('photo');
 
-photo.addEventListener('click', function() {
-  // здесь можно написать код, который будет выполняться при клике на фотографию
-  console.log('Клик на фотографии!');
-});
 
-// получаем ссылки на Canvas элемент и его контекст
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+// // получаем ссылки на Canvas элемент и его контекст
+// var canvas = document.getElementById('canvas');
+// var context = canvas.getContext('2d');
+// canvas.width = 800;
+// canvas.height = 800;
+// // загружаем изображение на Canvas элемент
+// var image = new Image();
+// image.src = 'case.webp';
+// image.width= 1000;
+// image.onload = function() {
+//   context.drawImage(image, 0, 0, canvas.width, canvas.height);
+//   context.fillStyle = 'black';
+//   context.font = '20px Arial';
+//   context.fillText('Мой текст', 10, canvas.height - 30);
+// };
+
+// // добавляем обработчик события для движения мыши
+// canvas.addEventListener('mousemove', function(event) {
+//   // проверяем, была ли нажата кнопка мыши
+//   if (event.buttons !== 1) {
+//     return;
+//   }
+
+//   // определяем координаты мыши на Canvas элементе
+//   var x = event.offsetX;
+//   var y = event.offsetY;
+
+//   // стираем пиксель на указанных координатах
+//   context.clearRect(x-15, y-15, 30, 30);
+// });
+
+// // добавляем обработчик события для нажатия кнопки мыши
+// canvas.addEventListener('mousedown', function(event) {
+//   // запрещаем выделение текста на Canvas элементе
+//   event.preventDefault();
+// });
+
+
+
+
+
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
 canvas.width = 800;
-canvas.height = 600;
-// загружаем изображение на Canvas элемент
-var image = new Image();
-image.src = 'zzBNjg3bBkg.jpg';
-image.width= 1000;
+canvas.height = 800;
+
+const image = new Image();
+image.src = 'case.webp';
 image.onload = function() {
-  context.drawImage(image, 0, 0, canvas.width, canvas.height);
-};
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+}
 
-// добавляем обработчик события для движения мыши
-canvas.addEventListener('mousemove', function(event) {
-  // проверяем, была ли нажата кнопка мыши
-  if (event.buttons !== 1) {
-    return;
-  }
-
-  // определяем координаты мыши на Canvas элементе
-  var x = event.offsetX;
-  var y = event.offsetY;
-
-  // стираем пиксель на указанных координатах
-  context.clearRect(x-15, y-15, 30, 30);
-});
-
-// добавляем обработчик события для нажатия кнопки мыши
-canvas.addEventListener('mousedown', function(event) {
-  // запрещаем выделение текста на Canvas элементе
-  event.preventDefault();
+canvas.addEventListener('touchmove', function(e) {
+  e.preventDefault(); // отключаем стандартное поведение браузера при касании
+  const touch = e.touches[0];
+  const x = touch.pageX - canvas.offsetLeft;
+  const y = touch.pageY - canvas.offsetTop;
+  ctx.clearRect(x-20, y-20, 40, 40);
 });
